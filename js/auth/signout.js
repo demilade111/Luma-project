01b03bef6edb1720537f00bc17ab3ceca40b9566
demilade111 <64733187+demilade111@users.noreply.github.com>
2025-06-24 21:06:29 -0700

    Implement navbar script import, enhance hero carousel functionality, and improve signout button event handling

--- js/auth/signout.js
+++ js/auth/signout.js
@@ -1,20 +1,19 @@
- 
- import { auth } from "../../config/firebase.js"; //
- import { signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
- 
- document.addEventListener("click", (e) => {
+import { auth } from "../../config/firebase.js";
+import { signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-    const signOutBtn = e.target.closest("#signOut")
+window.addEventListener("components-injected", () => {
+  const signOutBtn = document.getElementById("signOut");
 
-    if (signOutBtn) {
-            console.log("SignOut button CLICKED", signOutBtn);
-           signOut(auth)
-           .then(() => {
-            console.log("Signed out successfully.");
-               window.location.href = "/views/auth/login.html"
-           })
-            .catch((error) => {
-                console.error("Error signing out:", error);
-           });
-   }
+  if (!signOutBtn) {
+    return;
+  }
+
+  signOutBtn.addEventListener("click", async () => {
+    try {
+      await signOut(auth);
+      window.location.href = "/views/auth/login.html";
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
+  });
 });
